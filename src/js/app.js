bfff0ca4747ce877060b50f8c42560cd79900fd4
Joanna Biedroń <joanna.biedron@gmail.com>
2020-01-04 18:44:46 +0100

    add changes

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,51 @@
-import{settings, select,classNames, templates} from './settings.js';
+import{settings, select, classNames, templates} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 export const app = {
+initPages: function(){
+  const thisApp = this;
+
+  thisApp.pages = document.querySelector(select.containerOf.pages).children;
+  thisApp.navLinks = document.querySelectorAll(select.navLinks);
+
+  const idFromHash = window.location.hash.replace('#/', '');
+  console.log('idFromHash', idFromHash);
+
+  thisApp.activatePage(idFromHash);
+
+  for(let link of thisApp.navLinks){
+    link.addEventListener('click', function(event){
+      const clickedElement = this;
+      event.preventDefault();
+
+      //get id from href getAttribute
+      const id = clickedElement.getAttribute('href').replace('#', '');
+      //run thisApp.activatePage with that id
+      thisApp.activatePage(id);
+
+      //change URL hash
+      window.location.hash = '#/' + id;
+    });
+  }
+},
+
+  activatePage: function(pageId){
+    const thisApp = this;
+
+    //add class active to matching pages, remove from non-matching
+    for(let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+    //add class active to matching links, remove from non-matching
+    for(let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  },
+
   initMenu : function(){
     const thisApp = this;
     console.log('thisApp.data: ', thisApp.data);
@@ -42,6 +85,9 @@ export const app = {
     console.log('classNames:', classNames);
     console.log('settings:', settings);
     console.log('templates:', templates);
+
+    thisApp.initPages();
+
     thisApp.initData();
     //thisApp.initMenu();
     thisApp.initCart();
