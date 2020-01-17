c27d539be8022fc27d1ecc37831c9925f357cad5
Joanna Biedroń <joanna.biedron@gmail.com>
2020-01-17 15:29:53 +0100

    changes in code datepicker

--- src/js/components/DatePicker.js
+++ src/js/components/DatePicker.js
@@ -1,55 +1,54 @@
-
-import {select, settings} from '../settings.js';
-import {utils} from '../utils.js';
 import BaseWidget from './BaseWidget.js';
+import {settings, select} from '../settings.js';
+import {utils} from '../utils.js';
 
 class DatePicker extends BaseWidget{
   constructor(wrapper){
     super(wrapper, utils.dateToStr(new Date()));
 
     const thisWidget = this;
-
     thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.datePicker.input);
 
-    thisWidget.inputPlugin();
+    thisWidget.initPlugin;
   }
 
-  inputPlugin(){
+  initPlugin(){
 
     const thisWidget = this;
 
     thisWidget.minDate = new Date(thisWidget.value);
-    thisWidget.maxDate = utils.addDays(thisWidget.minDate,settings.datePicker.maxDaysInFuture);
-  }
+    thisWidget.maxDate = utils.addDays(thisWidget.minDate, settings.datePicker.maxDaysInFuture);
 
-  flatpickr(thisWidget.dom.input, {
+  /*  const options = {
       deafultDate: thisWidget.minDate,
       minDate: thisWidget.minDate,
       maxDate: thisWidget.maxDate,
-      locale:{
-        firstDayOfWeek: 1,
-      },
-      disable: [
-        function(date){
-          return(date.getDay() === 1);
+      'disable': [
+        function(date) {
+          // return true to disable
+          return (date.getDay() === 0 || date.getDay() === 6);
         }
       ],
+      'locale': {
+        'firstDayOfWeek': 1 // start week on Monday
+      },
       onChange: function (selectedDates, dateToStr) {
-       thisWidget.value = dateToStr;
-     }
+        thisWidget.value = dateToStr;
+      },
+    };
 
-    })
-
-  parseValue(){
-    return value;
+    flatpickr(thisWidget.dom.input, options);
+*/
   }
 
+  parseValue(value){
+    return value;
+  }
   isValid(){
     return true;
   }
-
   renderValue(){
-
+    console.log();
   }
 }
 export default DatePicker;
